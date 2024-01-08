794d9e37b620ba995f422986581d0dc1607c2dbe
Samuel McGraw III <smcgraw.net@gmail.com>
2024-01-08 09:54:04 -0500

    build up-to task-tabs

---
+++ app/App_Code/Handlers/Blob.generated.cs
@@ -0,0 +1,15 @@
+﻿namespace FreeTrial.Handlers
+{
+
+
+    public partial class BlobFactoryConfig : BlobFactory
+    {
+
+        public static void Initialize()
+        {
+            // register blob handlers
+            RegisterHandler("AtachmentsAttachment", "\"dbo\".\"Atachments\"", "\"Attachment\"", new string[] {
+                        "\"AttachmentID\""}, "Atachments Attachment", "Atachments", "Attachment");
+        }
+    }
+}
---
+++ app/App_Code/Services/ApplicationServices.generated.cs
@@ -0,0 +1,26 @@
+﻿using FreeTrial.Handlers;
+using FreeTrial.Data;
+using FreeTrial.Web;
+using System.Web.Configuration;
+
+namespace FreeTrial.Services
+{
+    public class AppFrameworkConfig
+    {
+
+        public virtual void Initialize()
+        {
+            ApplicationServices.FrameworkAppName = "Task Manager";
+            ApplicationServices.Version = "8.9.36.0";
+            ApplicationServices.HostVersion = "1.2.5.0";
+            var releaseMode = true;
+            AquariumExtenderBase.EnableMinifiedScript = releaseMode;
+            AquariumExtenderBase.EnableCombinedScript = releaseMode;
+            ApplicationServices.EnableMinifiedCss = releaseMode;
+            ApplicationServices.EnableCombinedCss = releaseMode;
+            CultureManager.SupportedCultures = new string[] {
+                    "en-US,en-GB"};
+            BlobFactoryConfig.Initialize();
+        }
+    }
+}
